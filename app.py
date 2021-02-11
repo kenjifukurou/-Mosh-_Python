09dea68eadf7d93594c9a9ecabb3b03876353ad5
Kenji Fukurou <59118789+kenjifukurou@users.noreply.github.com>
2021-02-11 17:26:55 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+# # print("mother fucker")
+
+# if 5 > 2:
+#     print("five is more than 2")
+
+# x = 5
+# y = "John "
+# z = "Cena"
+# print(x)
+# print(y)
+# print(y + z)
+
+# a = "awesome"
+# def awesomeFunc(): print(a)
+
+# awesomeFunc()
+
+# b = """this is lorem ipsum,
+# very long fzy,
+# bla by ly ta da. """
+
+# print(b)
+
+# for i in "arshivamarsaint":
+#     print(i)
+
+# sText = "Supre String"
+# print(sText[2:6])
+
+# paragraph = "lorem ipsum random text a b c d e"
+# print(paragraph.split(" "))
+
+#escape
+s = "trying escape character \'xyz\' test"
+print(s)
+
+s = "trying escape character \"xyz\" test"
+print(s)
+
+s = "trying escape character \\xyz\\ test"
+print(s)
+
+s = "trying escape character \nxyz\n test"
+print(s)
+
+s = "trying escape character \bxyz\b test"
+print(s)
+
+s = "trying escape character \fxyz\f test"
+print(s)
+
+s = "trying escape character \111xyz\111 test"
+print(s)
